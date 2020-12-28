ad7a1c3395cd876b4c72c0f4af4e3961a3b9fc17
Ilia Lesik <ilialesik@gmail.com>
2020-12-28 23:16:35 +0300

    Enable the plugin in .babelrc

--- src/App.tsx
+++ src/App.tsx
@@ -47,31 +47,31 @@ const SpaceFrame = () => {
       <Text style={styles.heading}>Space</Text>
 
       <Component name="SizingXSmall" style={styles.component}>
-        <View style={[styles.sizingBlock, { width: 8, height: 8 }]} />
+        <div style={[styles.sizingBlock, { width: 8, height: 8 }]} />
       </Component>
       <Text style={styles.sizingDescription}>X-Small</Text>
       <Text style={styles.hint}>8px</Text>
 
       <Component name="SizingSmall" style={styles.component}>
-        <View style={[styles.sizingBlock, { width: 16, height: 16 }]} />
+        <div style={[styles.sizingBlock, { width: 16, height: 16 }]} />
       </Component>
       <Text style={styles.sizingDescription}>Small</Text>
       <Text style={styles.hint}>16px</Text>
 
       <Component name="SizingMedium" style={styles.component}>
-        <View style={[styles.sizingBlock, { width: 24, height: 24 }]} />
+        <div style={[styles.sizingBlock, { width: 24, height: 24 }]} />
       </Component>
       <Text style={styles.sizingDescription}>Medium</Text>
       <Text style={styles.hint}>24px</Text>
 
       <Component name="SizingLarge" style={styles.component}>
-        <View style={[styles.sizingBlock, { width: 44, height: 44 }]} />
+        <div style={[styles.sizingBlock, { width: 44, height: 44 }]} />
       </Component>
       <Text style={styles.sizingDescription}>Large</Text>
       <Text style={styles.hint}>44px</Text>
 
       <Component name="SizingXLarge" style={styles.component}>
-        <View style={[styles.sizingBlock, { width: 64, height: 64 }]} />
+        <div style={[styles.sizingBlock, { width: 64, height: 64 }]} />
       </Component>
       <Text style={styles.sizingDescription}>X-Large</Text>
       <Text style={styles.hint}>64px</Text>

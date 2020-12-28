9119116655d3b430b1c9eb096812a3aa121b1dea
Ilia Lesik <ilialesik@gmail.com>
2020-12-28 23:13:03 +0300

    Replace ts-loader to babel-loader

--- webpack.hmr.config.js
+++ webpack.hmr.config.js
@@ -1,11 +1,10 @@
 var configure = require('react-figma-webpack-config/hmr')
 
 module.exports = configure({
-  //   resolve: {
-  //     extensions: ['.tsx', '.ts', '.jsx', '.js'],
-  //     alias: {
-  //       'react-figma$': '../../../src',
-  //       'react-figma/rpc$': '../../../src/rpc',
-  //     },
-  //   },
+    module: {
+        rules: [
+            // Converts TypeScript code to JavaScript
+            { test: /\.tsx?$/, use: 'babel-loader', exclude: /node_modules/ },
+        ]
+    },
 })

4d21558683cb2deb52d1c1b702db73c2fca8fb75
schickling <schickling.j@gmail.com>
2020-12-30 20:59:04 +0100

    Crashing snippet

--- src/NewApp.tsx
+++ src/NewApp.tsx
@@ -51,8 +51,8 @@ const SpaceFrame = () => {
       <Component name="SizingXSmall" style={styles.component}>
         <View style={[styles.sizingBlock, { width: 8, height: 8 }]} />
       </Component>
-      <span className="mt-1 text-sm">X-Small</span>
-      <span className="text-xs mt-0.5 text-gray-500">8px</span>
+      <div className="mt-1 text-sm">X-Small</div>
+      <div className="text-xs mt-0.5 text-gray-500">8px</div>
 
       <Component name="SizingSmall" style={styles.component}>
         <View style={[styles.sizingBlock, { width: 16, height: 16 }]} />
@@ -161,9 +161,30 @@ export const NewApp = () => {
   return (
     <Page isCurrent name="Design system" style={{ flexDirection: 'row' }}>
       <SpaceFrame />
-      <button className="inline-flex items-center px-6 py-3 bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 text-base font-medium text-white">
+      <button className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
         Button text
       </button>
+
+      <div className="bg-white">
+        <div className="px-4 py-12 mx-auto text-center max-w-7xl sm:px-6 lg:py-16 lg:px-8">
+          <div className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <div className="block">Ready to dive in?</div>
+            <div className="block">Start your free trial today.</div>
+          </div>
+          <div className="flex justify-center mt-8">
+            <div className="inline-flex rounded-md shadow">
+              <div className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700">
+                Get started
+              </div>
+            </div>
+            <div className="inline-flex ml-3">
+              <div className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200">
+                Learn more
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <TypeFrame style={{ marginLeft: 50 }} />
       <IconsFrame style={{ marginLeft: 50 }} />

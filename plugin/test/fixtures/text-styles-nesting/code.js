610b70d28cc3ac1afb2f4cf5296d7bbc60c2f72b
Ilia Lesik <ilialesik@gmail.com>
2021-01-04 15:16:00 +0300

    Implement text styles propagating
    Closes #6

---
+++ plugin/test/fixtures/text-styles-nesting/code.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Comp = props => {
+    return (
+        <div className="text-blue-800">
+            <div className="bg-blue-200 px-3 py-1 rounded-full font-semibold">
+                Hello Tailwind
+            </div>
+        </div>
+    );
+};

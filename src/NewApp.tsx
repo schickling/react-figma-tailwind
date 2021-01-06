55d13887620c7d61a5926fa345e30679874b826e
Ilia Lesik <ilialesik@gmail.com>
2021-01-06 14:12:36 +0300

    Add default font (Inter) to figma app

--- src/NewApp.tsx
+++ src/NewApp.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react'
-import { Page } from 'react-figma'
+import { Page, StyleSheet } from 'react-figma'
 import {Artboard} from "./Artboard";
+
+const styles = StyleSheet.create({
+    page: {
+        flexDirection: 'row',
+        fontFamily: "Inter"
+    }
+})
 
 export const NewApp = () => {
   return (
-    <Page isCurrent name="Tailwind" style={{ flexDirection: 'row' }}>
+    <Page isCurrent name="Tailwind" style={styles.page}>
       <Artboard />
     </Page>
   )
